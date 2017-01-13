fe7f84bb92db498edc671a5501b82f2f57246ab5
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2017-01-13 11:01:44 +0200

    code and properties refactoring

--- code/IoTnode.py
+++ code/IoTnode.py
@@ -5,122 +5,106 @@
 
 import time
 import math
-#Import library for the light sensor
+# Import library for the light sensor
 import light_library
 
-#Import temperature humidity sensor library
+# Import temperature humidity sensor library
 import grove_i2c_temp_hum_mini
 
-#Import grovepi
+# Import grovepi
 import grovepi
 
-#Import rabbit library
+# Import rabbitmq library
 import pika
-from properties import username
-from properties import password
-from properties import ip
-from properties import port
-from properties import exchange
-from properties import gateway
+import properties
 
-#Create the object for humidity and light 
-t=grove_i2c_temp_hum_mini.th02()
-l=light_library.light()
+exchange = properties.client_id + '-send'
 
-#digital inputs for PowerOff/On the temperature&humidty sensor
+# Create the object for humidity and light
+t = grove_i2c_temp_hum_mini.th02()
+l = light_library.light()
+
+# digital inputs for PowerOff/On the temperature&humidty sensor
 TH02_power = 4
-grovepi.pinMode(TH02_power,"OUTPUT")
+grovepi.pinMode(TH02_power, "OUTPUT")
 
-#values for loudness sensor
-ref_SPL = 94 
+# values for loudness sensor
+ref_SPL = 94
 sensitivity = 3.16
 loudness_sensor = 0
 
+# Initialize the connection with rabbit server
+credentials = pika.PlainCredentials(properties.client_id, properties.client_secret)
+parameters = pika.ConnectionParameters(properties.ip, properties.port, '/', credentials)
+
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+
+def publish(sensorName, value, timestamp=None):
+    if timestamp is None:
+        ts = int(time.time())
+        ts *= 1000
+        timestamp = str(ts)
+    body = (properties.client_id + '/' + properties.gateway + '/' + sensorName + ',' + value + ',' + timestamp)
+    channel.basic_publish(exchange=exchange, routing_key=exchange, body=body)
+    print("send:" + body)
+
 
 def main():
-	#Initialize the conection with rabbit server
-	credentials = pika.PlainCredentials(username, password)
-	parameters = pika.ConnectionParameters(ip,port,'/',credentials)
+    # Power off the temperature & humidity sensor
+    grovepi.digitalWrite(TH02_power, 1)
+    # initialize the light sensor
+    l.init()
+    # read sensor and update values on rabbit server
 
-	connection = pika.BlockingConnection(parameters)
-	channel =connection.channel()
+    # Temperature and Humidity
+    # while (True):
+    # Read temperature and humidity
+    grovepi.digitalWrite(TH02_power, 0)  # Power On the Tem&Hum module
+    time.sleep(.1)  # Wait to start I2C communication
+    tem = t.getTemperature()
+    hum = t.getHumidity()
+    # print("Temp: %.2fC\tHumidity:%.2f" %(tem,hum),"%")
+    time.sleep(.1)
+    grovepi.digitalWrite(TH02_power, 1)  # Power Off the Tem&Hum module
+    time.sleep(.1)
 
-	#Power off the temperature&humidity sensor
-	grovepi.digitalWrite(TH02_power,1)
-	#initialize the light sensor
-	l.init()
-	#read sensor and udate values on rabbit server
+    # Send temperature
+    value = "%.4f" % tem
+    publish('temp', value)
 
-	#Temperature and Humidity
-	#while (True):
-	#Read temperature and humidty
-	grovepi.digitalWrite(TH02_power,0) #Power On the Tem&Hum module
-	time.sleep(.1)	#Wait to start I2C comunication  
-	tem = t.getTemperature()
-	hum = t.getHumidity()
-	#print("Temp: %.2fC\tHumidity:%.2f" %(tem,hum),"%") 
-	time.sleep(.1)
-	grovepi.digitalWrite(TH02_power,1) #Power Off the Tem&Hum module
-	time.sleep(.1)
-	
-	#Send temperature
-	value = "%.4f" % tem
-	ts = int(time.time())
-	ts = ts*1000
-	timestamp=str(ts)
-	sensorName='temp'
-	body=(username+'/'+gateway+'/'+sensorName+','+value+','+timestamp)
-	channel.basic_publish(exchange=exchange,routing_key='hello',body=body)
-	print("send:"+body)
-	
-	#Send Humidty
-	value = "%.4f" % hum
-	ts = int(time.time())
-	ts = ts*1000
-	timestamp=str(ts)
-	sensorName='hum'
-	body=(username+'/'+gateway+'/'+sensorName+','+value+','+timestamp)
-	channel.basic_publish(exchange=exchange,routing_key='hello',body=body)
-	print("send:"+body)
-	
-	#Read from light module
-	lux=l.readVisibleLux()
-	time.sleep(0.1)
+    # Send Humidty
+    value = "%.4f" % hum
+    publish('hum', value)
 
-	#Send lux
-	value = "%.4f" % lux
-	ts = int(time.time())
-	ts = ts*1000
-	timestamp=str(ts)
-	sensorName='light'
-	body=(username+'/'+gateway+'/'+sensorName+','+value+','+timestamp)
-	channel.basic_publish(exchange=exchange,routing_key='hello',body=body)
-	print("send:"+body)
+    # Read from light module
+    lux = l.readVisibleLux()
+    time.sleep(0.1)
 
-	#Sound sensor read
-	try:
-		# Read the sound level
-		rms = grovepi.analogRead(loudness_sensor)
-		rms = rms*0.0032
-		if (rms <=0):
-			rms=0.0032
-		db_current = (ref_SPL + 20 * math.log10(rms/ sensitivity)); 
-		time.sleep(.1)
+    # Send lux
+    value = "%.4f" % lux
+    publish('light', value)
 
-		#send loudnes
-		value = "%.4f" % db_current
-		ts = int(time.time())
-		ts = ts*1000
-		timestamp=str(ts)
-		sensorName='sound'
-		body=(username+'/'+gateway+'/'+sensorName+','+value+','+timestamp)
-		channel.basic_publish(exchange=exchange,routing_key='hello',body=body)
-		print("send:"+body)			
+    # Sound sensor read
+    try:
+        # Read the sound level
+        rms = grovepi.analogRead(loudness_sensor)
+        rms *= 0.0032
+        if rms <= 0:
+            rms = 0.0032
+        db_current = (ref_SPL + 20 * math.log10(rms / sensitivity));
+        time.sleep(.1)
 
-   	except IOError:
-		print ("Error")	
-	#time.sleep(2)
-	connection.close()
-	
+        # send loudness
+        value = "%.4f" % db_current
+        publish('sound', value)
+
+    except IOError:
+        print ("Error")
+        # time.sleep(2)
+    connection.close()
+
+
 if __name__ == "__main__":
-        main()
+    main()
--- code/IoTnodepir.py
+++ code/IoTnodepir.py
@@ -2,49 +2,51 @@
 # IoTnode.py: Program a GAIA IoT node python for RaspberryPi over GrovePi bridge with a PIR sensor
 
 import time
-#Import grovepi
+# Import grovepi
 import grovepi
 
-#Import rabbit library
+# Import rabbitmq library
 import pika
-from properties import username
-from properties import password
-from properties import ip
-from properties import port
-from properties import exchange
-from properties import gateway
+import properties
+
+exchange = properties.client_id + '-send'
+routing_key = exchange
 
 pir_sensor = 8
-motion=0
-grovepi.pinMode(pir_sensor,"INPUT")
+motion = 0
+grovepi.pinMode(pir_sensor, "INPUT")
 
-credentials = pika.PlainCredentials(username, password)
-parameters = pika.ConnectionParameters(ip,port,'/',credentials)
+credentials = pika.PlainCredentials(properties.client_id, properties.client_secret)
+parameters = pika.ConnectionParameters(properties.ip, properties.port, '/', credentials)
 
 connection = pika.BlockingConnection(parameters)
-channel =connection.channel()
-mo=0
+channel = connection.channel()
+
+
+def publish(sensorName, value, timestamp=None):
+    if timestamp is None:
+        ts = int(time.time())
+        ts *= 1000
+        timestamp = str(ts)
+    body = (properties.client_id + '/' + properties.gateway + '/' + sensorName + ',' + value + ',' + timestamp)
+    channel.basic_publish(exchange=exchange, routing_key=exchange, body=body)
+    print("send:" + body)
+
+
+mo = 0
 while True:
-	try:
-	
-		# Sense motion, usually human, within the target range
-		motion=grovepi.digitalRead(pir_sensor)
-		if motion==0 or motion==1:	# check if reads were 0 or 1 it can be 255 also because of IO Errors so remove those values
-			if motion!=mo:
-					mo=motion
-					#Send Humidty
-					#value = "%.4f" % hum
-					value = str(mo)
-					ts = int(time.time())
-					ts = ts*1000
-					timestamp=str(ts)
-					sensorName='pir'
-					body=(username+'/'+gateway+'/'+sensorName+','+value+','+timestamp)
-					channel.basic_publish(exchange=exchange,routing_key='hello',body=body)
-					print("send:"+body)	
+    try:
+        # Sense motion, usually human, within the target range
+        motion = grovepi.digitalRead(pir_sensor)
+        if motion == 0 or motion == 1:  # check if readings were 0 or 1 it can be 255 also because of IO Errors so remove those values
+            if motion != mo:
+                mo = motion
+                # Send Motion
+                value = str(mo)
+                publish('pir', value)
 
-			# if your hold time is less than this, you might not see as many detections
-		time.sleep(.2)
+                # if your hold time is less than this, you might not see as many detections
+        time.sleep(.2)
 
-	except IOError:
-		print ("Error")
+    except IOError:
+        print ("Error")
